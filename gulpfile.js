63ea9739d56ecaf4d47fa3c4b0e9b36cbf2be76e
Josué Alarcón <josuealarco@gmail.com>
2022-08-31 15:34:39 -0500

    Gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,15 @@
+const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
+
+function buildStyles() {
+    return gulp.src('./source/sass/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./build/css'))
+}
+exports.buildStyles = buildStyles;
+exports.watch = function () {
+    gulp.watch('./source/sass/**/*.scss', buildStyles);
+  };
