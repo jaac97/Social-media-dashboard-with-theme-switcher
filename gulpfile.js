4c6e510796ef940adcd87e716331729fe83303d8
Josué Alarcón <josuealarco@gmail.com>
2022-09-01 11:23:04 -0500

    autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,16 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
 
 function buildStyles() {
     return gulp.src('./source/sass/**/*.scss')
         .pipe(sourcemaps.init())
+        .pipe(autoprefixer({
+            cascade: false
+        }))
         .pipe(sass().on('error', sass.logError))
-        .pipe(sourcemaps.write())
+        .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest('./build/css'))
 }
 exports.buildStyles = buildStyles;
